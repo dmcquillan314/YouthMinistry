a06f02c8f5db2d6dc60c00ae4122cd619fb80864
Dan McQuillan <dan@deepdishdev.com>
2013-02-14 23:59:01 -0600

    Modified foreign and primary key declarations

--- YouthMinistryDb.sql
+++ YouthMinistryDb.sql
@@ -3,7 +3,7 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 DROP SCHEMA IF EXISTS `YouthMinistry` ;
-CREATE SCHEMA IF NOT EXISTS `YouthMinistry` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+CREATE SCHEMA IF NOT EXISTS `YouthMinistry` DEFAULT CHARACTER SET utf16 COLLATE utf16_general_ci ;
 USE `YouthMinistry` ;
 
 -- -----------------------------------------------------
@@ -11,8 +11,8 @@ USE `YouthMinistry` ;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `YouthMinistry`.`group` ;
 
-CREATE  TABLE IF NOT EXISTS `YouthMinistry`.`group` (
-  `groupid` INT NOT NULL AUTO_INCREMENT ,
+CREATE TABLE IF NOT EXISTS `YouthMinistry`.`group` (
+  `groupid` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `group_name` VARCHAR(100) NOT NULL ,
   `group_desc` VARCHAR(255) NULL ,
   PRIMARY KEY (`groupid`) )
@@ -24,10 +24,11 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `YouthMinistry`.`groupmembers` ;
 
-CREATE  TABLE IF NOT EXISTS `YouthMinistry`.`groupmembers` (
-  `groupid` INT NOT NULL ,
-  `memberid` INT NOT NULL ,
+CREATE TABLE IF NOT EXISTS `YouthMinistry`.`groupmembers` (
+  `groupid` INT UNSIGNED NOT NULL ,
+  `memberid` INT UNSIGNED NOT NULL ,
   PRIMARY KEY (`groupid`, `memberid`) ,
+  KEY (`memberid`),
   INDEX `groupid_idx` (`groupid` ASC) ,
   CONSTRAINT `groupid`
     FOREIGN KEY (`groupid` )
@@ -42,8 +43,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `YouthMinistry`.`role` ;
 
-CREATE  TABLE IF NOT EXISTS `YouthMinistry`.`role` (
-  `roleid` INT NOT NULL AUTO_INCREMENT ,
+CREATE TABLE IF NOT EXISTS `YouthMinistry`.`role` (
+  `roleid` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `role_name` VARCHAR(100) NOT NULL ,
   `role_desc` VARCHAR(255) NULL ,
   PRIMARY KEY (`roleid`) )
@@ -55,13 +56,13 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `YouthMinistry`.`rolemembers` ;
 
-CREATE  TABLE IF NOT EXISTS `YouthMinistry`.`rolemembers` (
-  `roleid` INT NOT NULL ,
-  `memberid` INT NOT NULL ,
+CREATE TABLE IF NOT EXISTS `YouthMinistry`.`rolemembers` (
+  `roleid` INT UNSIGNED UNSIGNED NOT NULL ,
+  `memberid` INT UNSIGNED UNSIGNED NOT NULL ,
   PRIMARY KEY (`roleid`, `memberid`) ,
+  KEY (`memberid`),
   INDEX `groupid_idx` (`roleid` ASC) ,
-  CONSTRAINT `roleid`
-    FOREIGN KEY (`roleid` )
+  FOREIGN KEY (`roleid` )
     REFERENCES `YouthMinistry`.`role` (`roleid` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -73,27 +74,25 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `YouthMinistry`.`users` ;
 
-CREATE  TABLE IF NOT EXISTS `YouthMinistry`.`users` (
-  `memberid` INT NOT NULL AUTO_INCREMENT ,
+CREATE TABLE IF NOT EXISTS `YouthMinistry`.`users` (
+  `memberid` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `username` VARCHAR(45) NOT NULL ,
   `password` VARCHAR(45) NOT NULL ,
   `email` VARCHAR(100) NULL ,
   `first_name` VARCHAR(45) NOT NULL ,
   `last_name` VARCHAR(45) NOT NULL ,
-  `created` DATETIME NOT NULL DEFAULT 0000-00-00 00:00 ,
+  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` DATETIME NOT NULL ,
   PRIMARY KEY (`memberid`, `username`, `password`) ,
   UNIQUE INDEX `username_UNIQUE` (`username` ASC) ,
   INDEX `memberid_idx` (`memberid` ASC) ,
   INDEX `memberid_idx1` (`memberid` ASC) ,
-  CONSTRAINT `memberid`
-    FOREIGN KEY (`memberid` )
-    REFERENCES `YouthMinistry`.`groupmembers` (`groupid` )
+  FOREIGN KEY (`memberid` )
+    REFERENCES `YouthMinistry`.`groupmembers` (`memberid` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `memberid`
-    FOREIGN KEY (`memberid` )
-    REFERENCES `YouthMinistry`.`rolemembers` (`roleid` )
+  FOREIGN KEY (`memberid` )
+    REFERENCES `YouthMinistry`.`rolemembers` (`memberid` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
@@ -104,14 +103,13 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `YouthMinistry`.`eventgroup` ;
 
-CREATE  TABLE IF NOT EXISTS `YouthMinistry`.`eventgroup` (
-  `eventid` INT NOT NULL ,
-  `groupid` VARCHAR(45) NOT NULL ,
+CREATE TABLE IF NOT EXISTS `YouthMinistry`.`eventgroup` (
+  `eventid` INT UNSIGNED NOT NULL ,
+  `groupid` INT UNSIGNED NOT NULL ,
   PRIMARY KEY (`eventid`, `groupid`) ,
-  INDEX `groupid_idx` () ,
-  CONSTRAINT `groupid`
-    FOREIGN KEY ()
-    REFERENCES `YouthMinistry`.`group` ()
+  INDEX `groupid_idx` (`groupid` ASC) ,
+  FOREIGN KEY (`groupid`)
+    REFERENCES `YouthMinistry`.`group` (`groupid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -122,14 +120,13 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `YouthMinistry`.`event` ;
 
-CREATE  TABLE IF NOT EXISTS `YouthMinistry`.`event` (
-  `eventid` INT NOT NULL AUTO_INCREMENT ,
+CREATE TABLE IF NOT EXISTS `YouthMinistry`.`event` (
+  `eventid` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `event_name` VARCHAR(255) NOT NULL ,
   `event_desc` VARCHAR(255) NULL ,
   PRIMARY KEY (`eventid`) ,
   INDEX `eventid_idx` (`eventid` ASC) ,
-  CONSTRAINT `eventid`
-    FOREIGN KEY (`eventid` )
+  FOREIGN KEY (`eventid` )
     REFERENCES `YouthMinistry`.`eventgroup` (`eventid` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
